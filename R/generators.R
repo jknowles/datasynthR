da8e1da35b8480715c12339b028f7fdb4c88c5ec
Jared Knowles <jknowles@gmail.com>
2013-05-13 16:45:48 -0500

    binomial DV generator

--- R/generators.R
+++ R/generators.R
@@ -15,18 +15,14 @@ genNumeric <- function(n, k, names, rho, pattern){
 
 # Generate glm
 
-form <- list(coefs = rnorm(5), vars = sample(names(gdf), 5)) 
+
 
 # evaluate name?
 genBinomiaDV <- function(df, form, errors, intercept){
-  z <- intercept + 
+  # if form = NULL
+  form$exp <- parse(text=paste0(form$coefs, "*","gdf$", form$vars))
+  z <- intercept + eval(form$exp) + rnorm(dim(gdf)[1])
+  pr <- 1/(1+exp(-z))
+  y = rbinom(dim(gdf)[1], 1, pr)
+  return(y)
 }
-
-# 
-# z <- 3 + 2 * cov[, 1] + 3 * cov[, 2] - 8 * cov[, 3] +
-#   15 * cov[, 4] - 2 * cov[, 5]^2 + 12 * cov[, 27] + 
-#   .01 * cov[, 12] + .5 * cov[, 32] + - .0321 * cov[, 27] + rnorm(1000)
-# 
-# pr = 1/(1+exp(-z))         
-# y = rbinom(1000,1,pr)
-
--- inst/test-genNumeric.R
+++ inst/test-genNumeric.R
@@ -4,3 +4,8 @@ library(testthat)
 gdf <- genNumeric(2000, 50, rho=0.05)
 
 gdf <- as.data.frame(gdf)
+
+
+terms <- list(coefs = rnorm(5), vars = sample(names(gdf), 5)) 
+
+y <- genBinomiaDV(gdf, form=terms, intercept=3)
