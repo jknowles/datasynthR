d3410f19fb2ee30e54b41e0f4ee6ec8fc3b656cc
Jared Knowles <jknowles@gmail.com>
2013-05-17 12:52:33 -0500

    Add uniform and poisson correlation generators Add tests for these

--- R/correlatedsamplers.R
+++ R/correlatedsamplers.R
@@ -3,29 +3,32 @@
 #
 #################################################################################
 
-rnormcor <- function(x,rho) rnorm(1,rho*x,sqrt(1-rho^2))
+rnormcor <- function(x,rho) rnorm(1, rho*x, sqrt(1-rho^2))
 
 rchisqcor <- function(x, rho){
   sign(rho)*sign(x)*sum(rep(sapply(x, rnormcor, rho=rho), times=length(x))^2)
 }
 
-rpoiscor <- function(x, rho, scale){
+rpoiscor <- function(x, rho){
   tmp <- sapply(x, rnormcor, rho=rho)
-  y <- rpois(length(x), lambda=mean(tmp))
-  return(y)
+  b <- round(tmp,digits=0)
+  b <- b + abs(min(b))
+  b <- b^3
+  return(b)
 } 
 
-a <- rnorm(1000)
-tmp <- sapply(a, rnormcor, rho=0.7)
-y <- sapply(tmp, rpois, lambda=3)
 
-y <- sapply(tmp, rpois, lambda=(tmp+.01)^2)
+runifcor.cor <- function(x, rho){
+  hw <- function(r){ 
+    tmp <- (3-sqrt(1+8*abs(r)))/4 
+    return(tmp * sign(r))
+  } 
+  y <- (x*sign(rho) + runif(1000,-hw(abs(rho)),hw(rho*sign(rho)))) %% 1 
+  return(y)
+}
 
+# only works coming from uniform data
 
-
-b <- rpoiscor(a, rho=.1)
-
-cor(a,b)
 
 
 "unif", 
--- inst/test-correlatedsamplers.R
+++ inst/test-correlatedsamplers.R
@@ -18,8 +18,15 @@ test_that("Correlation is correct", {
   
 })
 
+test_that("Direction is correct", {
+  expect_equal(sign(cor(a,b)), sign(RHO1))
+  expect_equal(sign(cor(a,c)), sign(RHO2))
+  expect_equal(sign(cor(a,d)), sign(RHO3))
+})
 
-contxt("Chi-square")
+
+
+context("Chi-square")
 
 RHO1 <- -.5
 a <- rchisq(1000, df=12)
@@ -30,17 +37,69 @@ test_that("Correlation of chi-square is correct", {
   expect_equivalent(cor(a,b), RHO1)
 })
 
+test_that("Direction is correct", {
+  expect_equal(sign(cor(a,b)), sign(RHO1))
+  expect_equal(sign(cor(a,c)), sign(RHO2))
+  expect_equal(sign(cor(a,d)), sign(RHO3))
+})
 
+
+context("Poisson")
 
 
 a <- rnorm(1000)
-b <- sapply(a, rnormcor, 0.7)
+tmp <- sapply(a, rnormcor, rho=0.7)
+b <- round(tmp,digits=0)
+b <- b + abs(min(b))
+b <- b^3
+
+mean(b)
+sd(b)
+qplot(b,a)
 cor(a,b)
 
+test_that("Correlation of poisson is correct", {
+  expect_equivalent(cor(a,b), RHO1)
+})
+
+test_that("Direction is correct", {
+  expect_equal(sign(cor(a,b)), sign(RHO1))
+  expect_equal(sign(cor(a,c)), sign(RHO2))
+  expect_equal(sign(cor(a,d)), sign(RHO3))
+})
+
+test_that("Result is poisson distributed", {
+  expect_equivalent(mean(b), sd(b))
+  
+})
+
+context("Uniform")
+
+RHO1 <- 0.7
+RHO2 <- -0.7
+RHO3 <- 0.05
+
+a <- runif(1000)
+b <- runifcor.cor(a, RHO1)
+c <- runifcor.cor(a, RHO2)
+d <- runifcor.cor(a, RHO3)
 
 
-a <- rpois(1000, lambda=4)
-b <- sapply(a, rchisqcor, rho=-.5)
-cor(a,b)
+test_that("Correlation is correct", {
+  expect_equivalent(cor(a,b), RHO1)
+  expect_equivalent(cor(a,c), RHO2)
+  expect_equivalent(cor(a,d), RHO3)
+  
+})
 
-qplot(a,b) + geom_smooth()
+test_that("Direction is correct", {
+  expect_equal(sign(cor(a,b)), sign(RHO1))
+  expect_equal(sign(cor(a,c)), sign(RHO2))
+  expect_equal(sign(cor(a,d)), sign(RHO3))
+})
+
+test_that("Result is uniformly distributed", {
+  #expect_equivalent(mean(b), sd(b))
+  
+})
+
