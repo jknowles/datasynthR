21003464322a32599308bf616393961efe244cc8
Jared Knowles <jknowles@gmail.com>
2013-10-14 21:45:30 -0500

    Update documentation and prepping for package deployment

--- build.R
+++ build.R
@@ -1,10 +1,13 @@
 library(roxygen2)
 
-roclet <- rd_roclet()
-roc_out(roclet, "R/generators.R", ".")
-roc_out(roclet, "R/correlatedsamplers.R", ".")
-roc_out(roclet, "R/helpers.R", ".")
-roc_out(roclet, "R/missingness.R", ".")
+roxygenize(package.dir = ".", roxygen.dir=".")
+
+# 
+# roclet <- rd_roclet()
+# roc_out(roclet, "R/generators.R", ".")
+# roc_out(roclet, "R/correlatedsamplers.R", ".")
+# roc_out(roclet, "R/helpers.R", ".")
+# roc_out(roclet, "R/missingness.R", ".")
 
 
 
